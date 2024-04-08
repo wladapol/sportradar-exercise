21918881cee6e971ff66937032ecf00ce12c2628
Jan Paweł Piotrowski <jan.pawel.piotrowski@gmail.com>
2024-04-09 00:15:21 +0200

    Add implementation for Team class

---
+++ src/main/java/scoreboard/model/Team.java
@@ -0,0 +1,14 @@
+package scoreboard.model;
+
+public class Team {
+
+    private final String name;
+
+    public Team(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
