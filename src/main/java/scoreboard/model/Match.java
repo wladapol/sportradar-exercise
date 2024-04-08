a95cdcc86e6c35cd8dc862e479eddf34731d5b29
Jan Paweł Piotrowski <jan.pawel.piotrowski@gmail.com>
2024-04-09 00:33:47 +0200

    Add implementation for class Match with constructor with scores set to zero.

---
+++ src/main/java/scoreboard/model/Match.java
@@ -0,0 +1,32 @@
+package scoreboard.model;
+
+public class Match {
+    private final Team homeTeam;
+    private final Team awayTeam;
+    private int homeTeamScore;
+    private int awayTeamScore;
+
+    public Match(Team homeTeam, Team awayTeam) {
+        this.homeTeam = homeTeam;
+        this.awayTeam = awayTeam;
+        this.homeTeamScore = 0;
+        this.awayTeamScore = 0;
+    }
+
+    public Team getHomeTeam() {
+        return homeTeam;
+    }
+
+    public Team getAwayTeam() {
+        return awayTeam;
+    }
+
+    public int getHomeTeamScore() {
+        return homeTeamScore;
+    }
+
+    public int getAwayTeamScore() {
+        return awayTeamScore;
+    }
+
+}
