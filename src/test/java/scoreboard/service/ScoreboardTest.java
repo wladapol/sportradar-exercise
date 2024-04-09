0c42c860d9a263883a68049e945b8a542e0e6e2f
Jan Paweł Piotrowski <jan.pawel.piotrowski@gmail.com>
2024-04-09 11:26:44 +0200

    added test cases for calculating summary of matches in the scoreboard

--- src/test/java/scoreboard/service/ScoreboardTest.java
+++ src/test/java/scoreboard/service/ScoreboardTest.java
@@ -7,6 +7,8 @@ import scoreboard.exception.NegativeScoreNumberException;
 import scoreboard.model.Match;
 import scoreboard.model.Team;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ScoreboardTest {
@@ -102,6 +104,45 @@ class ScoreboardTest {
         String actualMessage = exception.getMessage();
 
         assertEquals(expectedMessage, actualMessage);
+    }
+
+    @Test
+    void testCalculateSummary() throws NegativeScoreNumberException, DuplicatedMatchException, MatchNotFoundException {
+        //Arrange
+        Scoreboard scoreboard = new Scoreboard();
+
+        Team mexico = new Team("Mexico");
+        Team canada = new Team("Canada");
+        Team spain = new Team("Spain");
+        Team brazil = new Team("Brazil");
+        Team germany = new Team("Germany");
+        Team france = new Team("France");
+        Team uruguay = new Team("Uruguay");
+        Team italy = new Team("Italy");
+        Team argentina = new Team("Argentina");
+        Team australia = new Team("Australia");
+
+        scoreboard.startMatch(mexico, canada);
+        scoreboard.startMatch(spain, brazil);
+        scoreboard.startMatch(germany, france);
+        scoreboard.startMatch(uruguay, italy);
+        scoreboard.startMatch(argentina, australia);
+
+        scoreboard.findMatch(mexico, canada).updateScore(0,5);
+        scoreboard.findMatch(spain, brazil).updateScore(10,2);
+        scoreboard.findMatch(germany, france).updateScore(2,2);
+        scoreboard.findMatch(uruguay, italy).updateScore(6,6);
+        scoreboard.findMatch(argentina, australia).updateScore(3,1);
+
+        //Act
+        List<Match> summary = scoreboard.calculateSummary();
+
+        //Assert
+        assertEquals(summary.get(0).getHomeTeam().getName(),uruguay.getName());
+        assertEquals(summary.get(1).getHomeTeam().getName(),spain.getName());
+        assertEquals(summary.get(2).getHomeTeam().getName(),mexico.getName());
+        assertEquals(summary.get(3).getHomeTeam().getName(),argentina.getName());
+        assertEquals(summary.get(4).getHomeTeam().getName(),germany.getName());
     }
 
 }
