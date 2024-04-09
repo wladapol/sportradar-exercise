b059f6dc054a4c33891b80f3ab62cca4257bd6aa
Jan Paweł Piotrowski <jan.pawel.piotrowski@gmail.com>
2024-04-09 05:22:24 +0200

    Add test case checking if started and not finished match is added to the scoreboard.

--- src/test/java/scoreboard/service/ScoreboardTest.java
+++ src/test/java/scoreboard/service/ScoreboardTest.java
@@ -1,7 +1,10 @@
 package scoreboard.service;
 
 import org.junit.jupiter.api.Test;
+import scoreboard.model.Match;
+import scoreboard.model.Team;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class ScoreboardTest {
@@ -13,6 +16,22 @@ class ScoreboardTest {
 
         //Assert
         assertTrue(scoreboard.getMatches().isEmpty());
+    }
+
+    @Test
+    void testUniqueMatchStartedInScoreboardShouldBeAddedToTheScoreboard() {
+        //Arrange
+        Scoreboard scoreboard = new Scoreboard();
+        Team homeTeam = new Team("Poland");
+        Team awayTeam = new Team("Germany");
+        Match match = new Match(homeTeam, awayTeam);
+
+        //Act
+        scoreboard.startMatch(homeTeam, awayTeam);
+
+        //Assert
+        assertEquals(scoreboard.getMatch(homeTeam, awayTeam), match);
+
     }
 
 }
