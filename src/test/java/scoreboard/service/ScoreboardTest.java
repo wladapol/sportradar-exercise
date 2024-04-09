75fa8bb15940a6748d13fa12879f98731b46610e
Jan Paweł Piotrowski <jan.pawel.piotrowski@gmail.com>
2024-04-09 08:14:26 +0200

    Add test cases for finishing match in the scoreboard.

--- src/test/java/scoreboard/service/ScoreboardTest.java
+++ src/test/java/scoreboard/service/ScoreboardTest.java
@@ -3,6 +3,7 @@ package scoreboard.service;
 import org.junit.jupiter.api.Test;
 import scoreboard.exception.DuplicatedMatchException;
 import scoreboard.exception.MatchNotFoundException;
+import scoreboard.exception.NegativeScoreNumberException;
 import scoreboard.model.Match;
 import scoreboard.model.Team;
 
@@ -62,6 +63,40 @@ class ScoreboardTest {
 
         //Assert
         Exception exception = assertThrows(MatchNotFoundException.class, () -> scoreboard.findMatch(homeTeam, awayTeam));
+
+        String expectedMessage = "Have not found match for home team Poland and away team Germany";
+        String actualMessage = exception.getMessage();
+
+        assertEquals(expectedMessage, actualMessage);
+    }
+
+    @Test
+    void testShouldDeleteMatchFromScoreboardWhenMatchIsFinished() throws DuplicatedMatchException, NegativeScoreNumberException, MatchNotFoundException {
+        //Arrange
+        Scoreboard scoreboard = new Scoreboard();
+        Team homeTeam = new Team("Poland");
+        Team awayTeam = new Team("Germany");
+        scoreboard.startMatch(homeTeam, awayTeam);
+        Match match = scoreboard.getMatches().get(0);
+        match.updateScore(1,2);
+
+        //Act
+        scoreboard.finishMatch(match);
+
+        //Assert
+        assertTrue(scoreboard.getMatches().isEmpty());
+    }
+
+    @Test
+    void testShouldThrowExceptionWhenTryToFinishNonExistentMatch() {
+        //Arrange
+        Scoreboard scoreboard = new Scoreboard();
+        Team homeTeam = new Team("Poland");
+        Team awayTeam = new Team("Germany");
+        Match match = new Match(homeTeam, awayTeam);
+
+        //Assert
+        Exception exception = assertThrows(MatchNotFoundException.class, () -> scoreboard.finishMatch(match));
 
         String expectedMessage = "Have not found match for home team Poland and away team Germany";
         String actualMessage = exception.getMessage();
